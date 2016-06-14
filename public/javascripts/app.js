06355df8453ffe6ac18b90fa28dc5e460b59682d
andrei1902 <andrei.ionita.1902@gmail.com>
2016-06-14 17:39:54 +0300

    Updates
    Updates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,6 @@
+var adressBook = [];
+var population = [];
+
 function buildGrid(rows, cols) {
     var rowNumber;
     var colNumber;
@@ -18,6 +21,8 @@ function buildGrid(rows, cols) {
         content += '<tr>';
         for (var j = 0; j < colNumber; j++) {
             content += '<td id="' + i + j + '" class="table-cell">' + ' id ' + i + j + '</td>';
+            adressBook.push({ id: i + '' + j });
+
         }
         content += '</tr>';
     }
@@ -26,5 +31,27 @@ function buildGrid(rows, cols) {
 }
 
 $(document).ready(function() {
-    buildGrid(5, 5);
+    buildGrid(4, 4);
+    population = generatePops(2, adressBook);
+    console.log(population);
 });
+
+function generatePops(numberOfPops, adressBook) {
+    var book = adressBook;
+    var totalPops;
+    var population = [];
+    if (numberOfPops) {
+        totalPops = numberOfPops
+    } else {
+        totalPops = 1;
+    }
+    for (var i = 0; i < totalPops; i++) {
+        var pop = {};
+        pop.name = 'pop_' + i;
+        var randy = Math.floor(Math.random() * book.length);
+        pop.location = book[randy].id;
+        book.splice(randy, 1);
+        population.push(pop);
+    }
+    return population;
+}
