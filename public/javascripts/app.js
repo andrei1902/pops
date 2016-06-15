1379a3e97075743e6703f77c3946b75381d20170
andrei1902 <andrei.ionita.1902@gmail.com>
2016-06-15 17:27:51 +0300

    Updates
    Updates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -20,7 +20,7 @@ function buildGrid(rows, cols) {
     for (var i = 0; i < rowNumber; i++) {
         content += '<tr>';
         for (var j = 0; j < colNumber; j++) {
-            content += '<td id="' + i + j + '" class="table-cell">' + ' id ' + i + j + '</td>';
+            content += '<td id="' + i + j + '" class="table-cell"></td>';
             adressBook.push({ id: i + '' + j });
 
         }
@@ -31,9 +31,11 @@ function buildGrid(rows, cols) {
 }
 
 $(document).ready(function() {
-    buildGrid(4, 4);
-    population = generatePops(2, adressBook);
-    console.log(population);
+    buildGrid(5, 5);
+    population = generatePops(12, adressBook);
+    drawPops(population);
+    rainBeepers(adressBook);
+    redrawGrid(adressBook);
 });
 
 function generatePops(numberOfPops, adressBook) {
@@ -45,13 +47,46 @@ function generatePops(numberOfPops, adressBook) {
     } else {
         totalPops = 1;
     }
+
     for (var i = 0; i < totalPops; i++) {
         var pop = {};
+        //asign pop name
         pop.name = 'pop_' + i;
         var randy = Math.floor(Math.random() * book.length);
-        pop.location = book[randy].id;
+
+        //assign pop location
+        pop.location = book[randy];
         book.splice(randy, 1);
         population.push(pop);
     }
     return population;
+}
+
+function drawPops(population) {
+    for (p in population) {
+        $('#' + population[p].location.id).html('<span>' + population[p].name + '</span>');
+        var id = population[p].location;
+    }
+}
+
+function rainBeepers(adressBook) {
+
+    for (a in adressBook) {
+        var randy = Math.floor(Math.random() * 3).toString();
+        if (randy === '0') {
+            adressBook[a].beeper = -1;
+        } else if (randy === '1') {
+            adressBook[a].beeper = 0;
+        } else {
+            adressBook[a].beeper = +1;
+        }
+    }
+}
+
+function redrawGrid(adressBook) {
+    for (a in adressBook) {
+        $('#' + adressBook[a].id).append('<br><span>Beeper count: ' + adressBook[a].beeper + '</span>');
+        console.log(adressBook.length);
+    }
+
 }
